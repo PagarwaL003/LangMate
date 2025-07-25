f65d5c3d3769cde5587d34c8a33541d91490a3aa
Piyush Agarwal <agpiyush162003@gmail.com>
2025-07-25 12:14:32 +0530

    Corrected baseURL

--- frontend/chatapp/src/lib/axios.js
+++ frontend/chatapp/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseUrl:"http://localhost:3000/api/v1",
+    baseURL:"http://localhost:3000/api/v1",
     withCredentials:true,   // send cookies with requests
 });
 

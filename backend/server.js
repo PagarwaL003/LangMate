11ae22d186e735c1cbcd63afd697a48ae20875c4
Piyush Agarwal <agpiyush162003@gmail.com>
2025-07-25 12:43:42 +0530

    Updated users route

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 
 // routes
 app.use("/api/v1/auth" , authRoutes);
-app.use("/api/v1/user" , userRoutes);
+app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/chat" , chatRoutes);
 
 

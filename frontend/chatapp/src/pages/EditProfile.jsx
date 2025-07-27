c60d1f1eaf3594fdd57c3460ca802712deb42b95
Piyush Agarwal <agpiyush162003@gmail.com>
2025-07-27 18:37:02 +0530

    Hook error solved

--- frontend/chatapp/src/pages/EditProfile.jsx
+++ frontend/chatapp/src/pages/EditProfile.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import useauthUser from "../hooks/useauthUser";
+import useauthUser from "../hooks/useAuthUser";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { LANGUAGES } from "../constants/index.js";

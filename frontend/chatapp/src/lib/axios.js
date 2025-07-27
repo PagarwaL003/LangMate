1f437747e1425ba30369644e11551d471ebab7e0
Piyush Agarwal <agpiyush162003@gmail.com>
2025-07-27 18:32:42 +0530

    Updated backend baseURL

--- frontend/chatapp/src/lib/axios.js
+++ frontend/chatapp/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:3000/api/v1",
+    baseURL:"https://lang-mate.vercel.app/api/v1",
     withCredentials:true,   // send cookies with requests
 });
 
